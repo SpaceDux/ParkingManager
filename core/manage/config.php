886edab63f3059d1289bed834b69900950b721c7
Ryan Williams <SpaceDux@users.noreply.github.com>
2017-10-25 00:31:49 +0100

    Baby steps
    Basically idk

---
+++ core/manage/config.php
@@ -0,0 +1,20 @@
+<?php
+	//Parking Manager made by Ryan Williams (BACKEND) & Daniel Pullan (FRONTEND)
+	
+	//Data Base Connection Configuration
+	
+	//Host Address
+	$dbHost = "127.0.0.1:9000";
+	//Mysql USername
+	$dbU = "root";
+	//Mysql password
+	$dbPw = "";
+	//Database name
+	$dbSel = "rkpm";
+	
+	
+	//Website Configuration
+	//Website URL
+	$url = "http://localhost:9000/ParkingManager";
+	
+?>
---
+++ init.php
@@ -0,0 +1,4 @@
+<?php
+	require_once '/core/manage/config.php'; //Require the config
+	
+	require_once '/core/manage/db.con.php'; //Require the db config
