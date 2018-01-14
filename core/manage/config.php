a84ea919f6830f51fbac0a860b76df56380750f0
SpaceDux <ryan.a.williams@outlook.com>
2018-01-14 21:54:30 +0000

    It begins
    Preparing Fenrir Template Engine to work alongside a new Parking Manager

---
+++ core/manage/config.php
@@ -0,0 +1,19 @@
+<?php
+// Configuration file for Krypton \\
+
+	//Server configuration (Database connection!)
+	$_CONFIG['mysql']['host'] = '127.0.0.1'; //The IP for the datebase server
+
+	$_CONFIG['mysql']['user'] = 'root'; //The username for the database server
+
+	$_CONFIG['mysql']['pass'] = '1123'; //The password for the datebase server
+
+	$_CONFIG['mysql']['db'] = 'rkpm'; //Select the database you want to connect too
+
+	//Site Configuration
+	$_CONFIG['site']['url'] = 'http://localhost/ParkingManager'; //Server URL example.com (DOES NOT END WITH /)
+
+	$_CONFIG['site']['name'] = 'Parking Manager'; //Website name, {SITE_NAME}
+
+	$_CONFIG['site']['template'] = 'PM2018'; //Krypton Template System, Select the Template Directory
+?>
---
+++ template/PM2018/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<HTML>
+  <HEAD>
+    <title>{SITE_NAME} | Hub</title>
+    <link rel="stylesheet" href="{URL}/template/{TPL}/assets/css/main.css">
+    <link rel="stylesheet" href="{URL}/template/{TPL}/assets/css/simple-grid.css">
+  </HEAD>
+  <BODY>
+    <div class="topNav">
+      <div class="UserPanel">
+        <div class="UserFace">
+          
+        </div>
+      </div>
+    </div>
+    <div class="sideNav">
+
+    </div>
+  </BODY>
+</HTML>
