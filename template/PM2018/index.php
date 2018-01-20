44575a4ebbebfead82bd1f5ebbe7804297aa499e
Ryan Williams <SpaceDux@users.noreply.github.com>
2018-01-20 01:31:10 +0000

    More template
    The boring part

--- template/PM2018/index.php
+++ template/PM2018/index.php
@@ -9,12 +9,23 @@
     <div class="topNav">
       <div class="UserPanel">
         <div class="UserFace">
-          
+          <img src="{URL}/template/{TPL}/assets/img/logo.png" width="100" height="70" alt=""></img>
+
         </div>
       </div>
     </div>
     <div class="sideNav">
-
+      <ul>
+        <li>
+          <a href="{URL}/index">Home</a>
+        </li>
+        <li>
+          <a href="{URL}/index">Yard Check</a>
+        </li>
+        <li>
+          <a href="{URL}/index">Accounts</a>
+        </li>
+      </ul>
     </div>
   </BODY>
 </HTML>
