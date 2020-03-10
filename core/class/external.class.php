4cb09e8afbd0fb41e7807a81504cfb90af51cf99
Ryan Williams <SpaceDux@users.noreply.github.com>
2020-03-10 10:23:06 +0000

    Portal API prep

--- core/class/external.class.php
+++ core/class/external.class.php
@@ -1,6 +1,16 @@
 <?php
   namespace ParkingManager;
   class External {
-    
+
+    protected $mysql;
+
+    function GetBookings()
+    {
+      $this->pm = new PM;
+
+      
+
+      $this->pm = null;
+    }
   }
 ?>
